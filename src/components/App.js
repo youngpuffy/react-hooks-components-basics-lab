bfed647df60a390aabcbf48ab6bad63ef539432e
Youngpuffy <levimuturi2@gmail.com>
2025-04-08 09:54:13 +0300

    creating components

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,13 @@ function Home() {
     </div>
   );
 }
+function About(){
+  return(
+    <div id ="about">
+    <h2>About</h2>
+    </div>
+  )
+}
 
 {/* write an <About> component here */}
 
@@ -23,6 +30,8 @@ function App() {
   return (
     <div>
       <NavBar />
+      <Home />
+      <About />
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
     </div>
